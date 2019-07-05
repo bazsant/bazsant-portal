4977adc88579d3e01604a76abdf146a83670cbef
Jhones Bazilio dos Santos <jhonesbdsantos@outlook.com.br>
2019-07-05 11:33:07 -0300

    mudando para projeto vazio do asp net

--- BazsantPortal/Program.cs
+++ BazsantPortal/Program.cs
@@ -8,7 +8,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
 
-namespace bazsant_portal
+namespace BazsantPortal
 {
     public class Program
     {
